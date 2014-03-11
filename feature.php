91adcde77c78136f674e58d8cebe74a79142af93
Graham Jarvis <graham.jarvis@freestone.co.uk>
2014-03-11 16:30:26 +0000

    Added feature.php

---
+++ feature.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: freestonef097
+ * Date: 11/03/2014
+ * Time: 16:21
+ */
+function sum($a ,$b) {
+    $num1 = $a;
+    $num2 = $b;
+    $result = $num1 + $num2;
+
+    return $result;
+}
+
